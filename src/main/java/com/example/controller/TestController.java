6532169c5e6e24b6e28dbe7524237e1c6c4fdeed
Tan Yawen <2749934612@qq.com>
2025-07-09 15:47:28 +0800

    redis的部分配置

--- src/main/java/com/example/controller/TestController.java
+++ src/main/java/com/example/controller/TestController.java
@@ -1,12 +1,23 @@
 package com.example.controller;
 
+import com.example.service.SeatServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Autowired
+    private SeatServiceImpl seatService;
     @RequestMapping("/test")
     public String test() {
         return "hello world";
     }
+    @RequestMapping("/test/redis")
+    public String testredis() {
+        seatService.testRedis();
+        return "success";
+    }
+
 }
--- src/main/java/com/example/service/SeatServiceImpl.java
+++ src/main/java/com/example/service/SeatServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.service;
 import com.example.domain.entity.SeatEntity;
 import com.example.mapper.SeatMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,5 +22,14 @@ public class SeatServiceImpl {
 
     public int getSeatCountByTrainid(Integer trainId){
         return seatMapper.getSeatCountByTrainid(trainId);
+    }
+
+
+
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+    public void testRedis(){
+        redisTemplate.opsForValue().set("canal_test_redis", "hello world");
     }
 }
